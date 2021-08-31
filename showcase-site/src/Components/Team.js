3796b574526a7a1022d0d503019d72752e1f6680
mathieugery <mathieu.gery@epitech.eu>
2021-08-31 02:01:19 +0200

    Team section

--- showcase-site/src/Components/Team.js
+++ showcase-site/src/Components/Team.js
@@ -1,13 +1,64 @@
-import Mat from '../Constants/mat.jpg'
+import Mat from '../Constants/mat.jpeg'
+import Gregoire from '../Constants/gregoire.jpeg'
+import Romain from '../Constants/romain.jpeg'
+import Pierre from '../Constants/pierre.jpeg'
+import Mahe from '../Constants/mahe.jpeg'
+import Abel from '../Constants/abel.jpeg'
+import Steeven from '../Constants/steeven.jpeg'
+import Baptiste from '../Constants/baptiste.jpeg'
+
 import {GlobeAltIcon} from "@heroicons/react/outline";
 
 const people = [
     {
+        name: 'Grégoire Lanfranchini',
+        role: 'CEO & Développeur Front',
+        imageUrl: Gregoire,
+        linkedinUrl: 'https://www.linkedin.com/in/grégoire-lanfranchini-90b76015a/?originalSubdomain=fr',
+    },
+    {
         name: 'Mathieu Gery',
         role: 'DevOps, Admin Sys & fullstack',
         imageUrl: Mat,
+        twitterUrl: 'https://mathieu.gery.me',
+        linkedinUrl: 'https://www.linkedin.com/in/mathieu-gery-a75547157/',
+    },
+    {
+        name: 'Steeven Regnault',
+        role: 'Développeur Backend & DevOps',
+        imageUrl: Steeven,
         twitterUrl: '#',
-        linkedinUrl: '#',
+        linkedinUrl: 'https://www.linkedin.com/in/steeven-regnault/',
+    },
+    {
+        name: 'Pierre Chaudron',
+        role: 'Développeur Backend',
+        imageUrl: Pierre,
+        linkedinUrl: 'https://www.linkedin.com/in/pierre-chaudron-35754515a/',
+    },
+    {
+        name: 'Romain Floriani',
+        role: 'Développeur Backend',
+        imageUrl: Romain,
+        linkedinUrl: 'https://www.linkedin.com/in/romain-floriani/',
+    },
+    {
+        name: 'Abel Vinuela-Hamoudi',
+        role: 'Développeur Mobile',
+        imageUrl: Abel,
+        linkedinUrl: 'https://www.linkedin.com/in/abel-vinuela-hamoudi-3124a4151/',
+    },
+    {
+        name: 'Baptiste Gobet',
+        role: 'Développeur Mobile',
+        imageUrl: Baptiste,
+        linkedinUrl: 'https://www.linkedin.com/in/baptiste-gobet/',
+    },
+    {
+        name: 'Mahé Caurette',
+        role: 'Développeur Front',
+        imageUrl: Mahe,
+        linkedinUrl: 'https://www.linkedin.com/in/mahé-caurette-5ab2a015b/',
     },
     // More people...
 ]
@@ -17,7 +68,7 @@ export default function Team() {
             <div className="max-w-7xl mx-auto py-12 px-4 text-center sm:px-6 lg:px-8 lg:py-24">
                 <div className="space-y-12">
                     <div className="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl">
-                        <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl">qui sommes-nous ?</h2>
+                        <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl">Qui sommes-nous ?</h2>
                         <p className="text-xl text-gray-500">
                             Nous sommes une équipe de 8 développeurs d'Epitech Lille, par défaut tous développeurs Fullstack.
                             Certains membres ont des capacités techniques plus approfondies dans certains domaines.
@@ -37,6 +88,7 @@ export default function Team() {
                                             <p className="text-indigo-600">{person.role}</p>
                                         </div>
                                         <ul role="list" className="flex justify-center space-x-5">
+                                            {person.twitterUrl ?
                                             <li>
                                                 <a href={person.twitterUrl} className="text-gray-400 hover:text-gray-500">
                                                     <span className="sr-only">Twitter</span>
@@ -44,7 +96,7 @@ export default function Team() {
                                                         <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
                                                     </svg>
                                                 </a>
-                                            </li>
+                                            </li> : <div/>}
                                             <li>
                                                 <a href={person.linkedinUrl} className="text-gray-400 hover:text-gray-500">
                                                     <span className="sr-only">LinkedIn</span>
--- showcase-site/src/index.js
+++ showcase-site/src/index.js
@@ -5,10 +5,10 @@ import Home from './Pages/home'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Home/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
