c87c725a67fcfc537d1579d83c00e2b56990552c
mathieugery <mathieu.gery@epitech.eu>
2021-08-31 02:32:47 +0200

    Contact

--- showcase-site/src/Components/Contact.js
+++ showcase-site/src/Components/Contact.js
@@ -3,7 +3,7 @@ import { MailIcon, PhoneIcon } from '@heroicons/react/outline'
 
 export default function Contact() {
     return (
-        <div className="bg-white">
+        <div className="bg-green-600 bg-opacity-40">
             <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
                 <div className="max-w-lg mx-auto md:max-w-none md:grid md:grid-cols-2 md:gap-8">
                     <div>
